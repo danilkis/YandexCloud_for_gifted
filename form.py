85fb458ca33799ed759956dbf49cafe52dc7a558
danon <danyapancresnicov@gmail.com>
2023-04-26 10:39:55 +0300

    Json

--- form.py
+++ form.py
@@ -1,12 +1,17 @@
 import re
 from datetime import datetime
+import json
 import pdb
 from bottle import route, run, template, static_file, request
-NAMES = []
-MAILS = []
+
+# Функция для записи данных в файл JSON
+def write_to_json(data):
+    with open('data.json', 'a') as file:
+        json.dump(data, file)
+        file.write('\n')
+
 @route('/', method='post')
 def my_form():
-    message = " "
     # Получение полей ввода из формы
     mail = request.forms.get('email')
     name = request.forms.get('name')
@@ -18,15 +23,22 @@ def my_form():
         message = "Неверный email. Попробуйте еще раз"
 
     # Вывод ошибки при не заполненных полях
-    if not name or not mail:
+    elif not name or not mail:
         message = "Введите имя и почту!"
-    date = str(datetime.today().strftime("%Y-%m-%d"))
-    # Return a message with the user's name, email, and date
-    if mail and name and mail_check:
-        NAMES.append(name)
-        MAILS.append(mail)
-        message = f"Спасибо, {name}! Новости будут приходить на {mail}, Время обращения {date}"
-
+    else:
+        date = str(datetime.today().strftime("%Y-%m-%d"))
+        # Запись данных в файл JSON
+        data = {"name": name, "mail": mail, "date": date}
+        with open('data.json', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                # Проверка на дубликаты почты и имени
+                if json.loads(line)['mail'] == mail:
+                    message = "Пользователь с такой почтой уже существует!"
+                    break
+            else:
+                write_to_json(data)
+                message = f"Спасибо, {name}! Новости будут приходить на {mail}, Время обращения {date}"
     # Вывод страницы с сообщением
-    pdb.set_trace()
+    #pdb.set_trace()
     return template('home.tpl', message=message)
